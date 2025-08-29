7c95cd02ce44268d428cb5df4a83493e490c9758
Jamie Harvey <jamieharvey237@gmail.com>
2025-08-29 14:35:54 +0800

    Update Stage 2: Clean up by adding functions
    Put the captain selector into a function rather than the main script so it can easily be distinguished and called. Unified the points calculator into one function and introduced a new "position" variable to select which formula to use. Intention is for these functions to eventually extract specific elements of a dataframe to get the desired outputs.

--- Code_Monte_Carlo_FPL.py
+++ Code_Monte_Carlo_FPL.py
@@ -1,122 +1,138 @@
 import numpy as np
 
-#Define Fixture Difficulty Rating----------------------------------
-#introducing a coefficient which alters expected points based on quality of opposition using FDR
-FDR_coefficient=np.array([1.2,1,0.95,0.85])
-
-#Points calculators-------------------------------------------------
-def points_fwd(xg, xa, FDR): #Points calculation for a forward
-    goals = np.random.poisson(xg)
-    assists = np.random.poisson(xa)
-    points = FDR_coefficient[FDR-2]*((goals * 6) + (assists * 3) + 2) # constant +2 for appearance
-    return points
-
-def points_mid(xg, xa, xcs, FDR): #Assumption of no DC for attacking midfielders for now
-    goals = np.random.poisson(xg)
-    assists = np.random.poisson(xa)
-    clean_sheet = np.random.binomial(1, p=xcs) #binomial dist for success/failure outcomes
-    points = FDR_coefficient[FDR-2]*((goals * 5) + (assists * 3) + (1*clean_sheet) + 2) # constant +2 for appearance
-    return points
-
+# Run simulations for different values of n
+n_sims = 1000000
 
 #Player stats---------------------------------------------------------
-#haaland_xg = 0.75 #Data from "Footystats"
-#haaland_xa = 0.11
+#replace with data frame at future stage
+haaland_xg = 0.75 
+haaland_xa = 0.11
+haaland_pos="FWD"
+mci_xcs=12/38
+mci_gw_fdr=3
 
 salah_xg=0.72
 salah_xa=0.41
+salah_pos="MID"
 liv_xcs=14/38 #expected clean sheets
 liv_gw_fdr=4 #chelsea gameweek fixture difficulty rating
 
 palmer_xg=0.5
 palmer_xa=0.3
+palmer_pos="MID"
 che_xcs= 10/38
 che_gw_fdr=3 #chelsea gameweek fixture difficulty rating
 
-# Run simulations for different values of n
-n_sims = 1000000
-
-#Create set of points returns
-salah_sim_points = np.array([points_mid(salah_xg,salah_xa,liv_xcs,liv_gw_fdr) for _ in range(n_sims)])
-palmer_sim_points = np.array([points_mid(palmer_xg,palmer_xa,che_xcs,che_gw_fdr) for _ in range(n_sims)])
-
-#Calculate key metrics to decide best captain -----------------------------------------
-salah_metric_wins=0 #counter for how many categories each player wins in
-palmer_metric_wins=0
-
-# 1. Highest Average Points
-salah_avg=salah_sim_points.mean()
-palmer_avg=palmer_sim_points.mean()
-if salah_avg>palmer_avg:
-    salah_metric_wins+=1
-    print("Higher points average: Salah")
-else:
-    palmer_metric_wins+=1
-    print("Higher points average: Palmer")
+#Points calculators-------------------------------------------------
+def points_calc(pos,xg,xa,xcs,FDR):
+    FDR_coefficient=np.array([1.2,1,0.95,0.85]) # Can be edited to be more conservative
     
-#2. Likelihood of "haul"
-haul=10
-salah_haul=np.sum(salah_sim_points>=haul)
-palmer_haul=np.sum(palmer_sim_points>=haul)
-if salah_haul>palmer_haul:
-    salah_metric_wins+=1
-    print("Higher haul count: Salah")
-elif salah_haul<palmer_haul:
-    palmer_metric_wins+=1
-    print("Higher haul count: Palmer")
-else:
-    print("Equal hauls")
-
-#3. Likelihood of "blank"
-blank=2
-salah_blank=np.sum(salah_sim_points<=blank)
-palmer_blank=np.sum(palmer_sim_points<=blank)
-if salah_blank>palmer_blank:
-    palmer_metric_wins+=1
-    print("Lower blank count: Palmer")
-elif salah_blank<palmer_blank:
-    salah_metric_wins+=1
-    print("Lower blank count: Salah")
-else:
-    print("Equal blanks")
+    goals = np.random.poisson(xg)
+    assists = np.random.poisson(xa)
+    clean_sheet = np.random.binomial(1, p=xcs) #binomial dist for success/failure outcomes
     
-#4. Volatility of points (riskiness)---------------------------------------
-salah_std=np.std(salah_sim_points)
-palmer_std=np.std(palmer_sim_points)
-if salah_std>palmer_std:
-    palmer_metric_wins+=1
-    print("Less volatile: Palmer")
-elif salah_std<palmer_std:
-    salah_metric_wins+=1
-    print("Less volatile: Salah")
-else:
-    print("Equal volatility")
-    
-#5. Risk adjusted points return metric------------------------------------
-salah_risk_adj_points=salah_avg/salah_std
-palmer_risk_adj_points=palmer_avg/palmer_std
-
-if salah_risk_adj_points>palmer_risk_adj_points:
-    salah_metric_wins+=1
-    print("Best risk-adjusted points return: Salah")
-elif salah_risk_adj_points<palmer_risk_adj_points:
-    palmer_metric_wins+=1
-    print("Best risk-adjusted points return: Palmer")
-else:
-    print("Equal risk-adjusted points return")
-    
-#Best captain based on overall metric wins---------------------------------
-if salah_metric_wins>palmer_metric_wins:
-    print("Captain Selection: Salah")
-elif salah_metric_wins<palmer_metric_wins:
-    print("Captain Selection: Palmer")
-else:
-    if salah_avg>palmer_avg:
-        print("Captain Selection: Salah")
-    elif palmer_avg>salah_avg:
-        print("Captain Selection: Palmer")
+    if pos == "FWD":
+        points = round(FDR_coefficient[FDR-2]*((goals * 4) + (assists * 3) + 2)) # constant +2 for appearance
+    elif pos == "MID":
+        points = round(FDR_coefficient[FDR-2]*((goals * 5) + (assists * 3) + (1*clean_sheet) + 2)) # constant +2 for appearance
+    elif pos == "DEF":
+        points = round(FDR_coefficient[FDR-2]*((goals * 6) + (assists * 3) + (4*clean_sheet) + 2)) # constant +2 for appearance
+    elif pos == "GKP":
+        points = round(FDR_coefficient[FDR-2]*((goals * 10) + (assists * 3) + (4*clean_sheet) + 2)) # constant +2 for appearance
     else:
-        print("Captain Selection Inconclusive")
+        "Error in calculating points"
+    return points
+        
+def captain_selector(p1, p1_pos, xg_p1, xa_p1, xcs_p1, fdr_p1, p2, p2_pos, xg_p2, xa_p2, xcs_p2, fdr_p2): #need to put player names and position in "" string format
+    p1_sim_points=np.array([points_calc(p1_pos,xg_p1,xa_p1,xcs_p1,fdr_p1) for _ in range(n_sims)])
+    p2_sim_points=np.array([points_calc(p2_pos,xg_p2,xa_p2,xcs_p2,fdr_p2)  for _ in range(n_sims)])
+    
+    p1_wins=0 #metric tally initialisation
+    p2_wins=0
+    
+    # 1. Highest Average Points
+    p1_avg=p1_sim_points.mean()
+    p2_avg=p2_sim_points.mean()
+    if p1_avg>p2_avg:
+        p1_wins+=1
+        print("Higher points average:", p1)
+    else:
+        p2_wins+=1
+        print("Higher points average:", p2)
+        
+    #2. Likelihood of "haul"
+    haul=10
+    p1_haul=np.sum(p1_sim_points>=haul)
+    p2_haul=np.sum(p2_sim_points>=haul)
+    if p1_haul>p2_haul:
+        p1_wins+=1
+        print("Higher haul count:", p1)
+    elif p1_haul<p2_haul:
+        p2_wins+=1
+        print("Higher haul count:", p2)
+    else:
+        print("Equal hauls")
+    
+    #3. Likelihood of "blank"
+    blank=2
+    p1_blank=np.sum(p1_sim_points<=blank)
+    p2_blank=np.sum(p2_sim_points<=blank)
+    if p1_blank>p2_blank:
+        p2_wins+=1
+        print("Lower blank count:", p2)
+    elif p1_blank<p2_blank:
+        p1_wins+=1
+        print("Lower blank count:", p1)
+    else:
+        print("Equal blanks")
+    
+    #4. Volatility of points (riskiness)---------------------------------------
+    p1_std=np.std(p1_sim_points)
+    p2_std=np.std(p2_sim_points)
+    if p1_std>p2_std:
+        p2_wins+=1
+        print("Less volatile:", p2)
+    elif p1_std<p2_std:
+        p1_wins+=1
+        print("Less volatile:", p1)
+    else:
+        print("Equal volatility")
+        
+    #5. Risk adjusted points return metric------------------------------------
+    p1_risk_adj_points=p1_avg/p1_std
+    p2_risk_adj_points=p2_avg/p2_std
+
+    if p1_risk_adj_points>p2_risk_adj_points:
+        p1_wins+=1
+        print("Best risk-adjusted points return:", p1)
+    elif p1_risk_adj_points<p2_risk_adj_points:
+        p2_wins+=1
+        print("Best risk-adjusted points return:", p2)
+    else:
+        print("Equal risk-adjusted points return")
+    
+    #Best captain based on overall metric wins---------------------------------
+    if p1_wins>p2_wins:
+        captain=p1
+        print("Captain Selection:", p1)
+    elif p1_wins<p2_wins:
+        captain=p2
+        print("Captain Selection:", p2)
+    else:
+        if p1_avg>p2_avg:
+            captain=p1
+            print("Captain Selection:", p1)
+        elif p2_avg>p1_avg:
+            captain=p1
+            print("Captain Selection:", p2)
+        else:
+            captain="N/A"
+            print("Captain Selection Inconclusive")
+    
+    return captain
+
+captain=captain_selector("SALAH", salah_pos,salah_xg,salah_xa,liv_xcs,liv_gw_fdr,"PALMER", palmer_pos,palmer_xg,palmer_xa,che_xcs,che_gw_fdr)
+
 
 
 
